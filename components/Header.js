609e4c16d43cf5ba8249cfd8fd24f8b92c6e96cb
Uranus <sd20000507@gmail.com>
2021-10-02 10:56:13 +0530

    Add spinning wheel and change menus and joints bg

--- components/Header.js
+++ components/Header.js
@@ -1,25 +1,15 @@
 import styles from '../styles/Header.module.scss'
 import Image from 'next/image'
 import logo from '../public/logo.png'
-import { Button, Typography } from "@material-ui/core";
-import { useEffect, useRef } from 'react';
+import { Typography } from "@material-ui/core";
 
-export default function Header(props){
-
-  useEffect(()=>{
-    props.handleHeight(header.current.offsetHeight)
-  })
-  const header = useRef(null)
+export default function Header(){
     return(
-        <div ref={header} className={styles.header}>
-          <div className={styles.header__main}>
-            <Image width={160} height={120} src={logo} alt="The Menu"/>
-            <Typography variant="h1" gutterBottom={true}>Eat . Play . Share</Typography>
-          </div>
-          {/* <form action="/" className="d-flex">
-            <input type="search" placeholder="Search" aria-label="Search"/>
-            <Button color="primary" variant="contained" type="submit">Search</Button>
-          </form> */}
+        <div className={styles.header}>
+            <div className={styles.header__main}>
+                <Image width={160} height={120} src={logo} alt="The Menu"/>
+                <Typography variant="h1" gutterBottom={true}>Eat . Play . Share</Typography>
+            </div>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -14,11 +14,8 @@ import { useState, useRef, useEffect } from "react";
 export default function HomePage() {
     let theme = createTheme();
     theme = responsiveFontSizes(theme);
-    const tabs = useRef(null)
-    const tabsPanel = useRef(null)
 
     const [tab, setTab] = useState(0)
-    const [headerHeight, setHeaderHeight] = useState(0)
 
     return (
         <MuiThemeProvider theme={theme}>
@@ -28,9 +25,8 @@ export default function HomePage() {
                     <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={true}/>
                     <link href="https://fonts.googleapis.com/css2?family=Rock+Salt&family=Varela+Round&display=swap" rel="stylesheet"/>
                 </Head>
-                <Header handleHeight={(height) => setHeaderHeight(height)}/>
+                <Header/>
                 <Tabs
-                innerRef={tabsPanel}
                     indicatorColor="secondary"
                     textColor="secondary"
                     centered
@@ -42,14 +38,12 @@ export default function HomePage() {
                     <Tab icon={<RestaurantMenuOutlined/>} label="Foods"></Tab>
                     <Tab icon={<BookOutlined/>} label="Blogs"></Tab>
                 </Tabs>
-                <div ref={tabs}>
-                    <SwipableViews style={styles.tabs} axis="x" index={tab} onChangeIndex={(index) => setTab(index)}>
-                        <HomeTab hidden={tab!==0}/>
-                        <GamesTab hidden={tab!==1}/>
-                        <MenusTab hidden={tab!==2}/>
-                        <BlogsTab hidden={tab!==3}/>
-                    </SwipableViews>
-                </div>
+                <SwipableViews axis="x" index={tab} onChangeIndex={(index) => setTab(index)}>
+                    <HomeTab hidden={tab!==0}/>
+                    <GamesTab hidden={tab!==1}/>
+                    <MenusTab hidden={tab!==2}/>
+                    <BlogsTab hidden={tab!==3}/>
+                </SwipableViews>
                 <Footer />
             </div>
         </MuiThemeProvider>
